add158393f4fae0a91e7b8087d16518062749e1a
zer0warm <dqhieu.2016@gmail.com>
2024-11-20 11:52:04 +0700

    update: test.sh can accept arbitrary flags
    Send arbitrary options/flags to python3 -m unittest discover script.
    
    - update(test.sh):
      - Check for existence of extra flags
      - Also echo the command when execute

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-python3 -m unittest discover --verbose -s src
+if [ -z "$*" ]; then
+	(set -x; python3 -m unittest discover --verbose -s src)
+else
+	(set -x; python3 -m unittest discover --verbose -s src "$@")
+fi
