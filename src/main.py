dcd42a553384f2f849d40dd11dfe8ed1185cd91a
zer0warm <dqhieu.2016@gmail.com>
2024-11-25 21:34:58 +0700

    add: Recursively copy new static assets to public
    Copy static assets to public/ folder when `main.sh` is run, i.e. on
    launch.
    
    - update(main.py): recursive delete, copy files and create directories

--- src/main.py
+++ src/main.py
@@ -1,8 +1,38 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+
+def copy_files(src, dst):
+    print(f'Copying files from {src} to {dst}...')
+
+    if not os.path.isdir(src):
+        raise Exception(f'{src} does not exist in the current directory')
+
+    if os.path.isdir(dst):
+        print(f'{dst} exist, deleting...')
+        shutil.rmtree(dst)
+    if not os.path.isdir(dst):
+        print(f'{dst} not exist, creating...')
+        os.mkdir(dst)
+
+    for file in os.listdir(src):
+        entry = os.path.join(src, file)
+        print(f'<> Processing {entry}...')
+        if os.path.isdir(entry):
+            print(f'--> is a directory')
+
+            new_dst = os.path.join(dst, file)
+            print(f'--> creating {new_dst}...')
+            os.mkdir(new_dst)
+
+            new_src = os.path.join(src, file)
+            copy_files(new_src, new_dst)
+        if os.path.isfile(entry):
+            print(f'--> is a file')
+            print(f'--> copying {entry} to {dst}...')
+            shutil.copy(entry, dst)
 
 def main():
-    node = TextNode('This is SPARTA!!!', TextType.BOLD)
-    print(node)
+    copy_files('./static', './public')
 
 if __name__ == '__main__':
     main()
